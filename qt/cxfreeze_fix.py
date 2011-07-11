ad2d225a12ef55b796d7ff016c4613b3d522e0a0
Virgil Dupras <hsoft@hardcoded.net>
2011-07-11 22:41:17 +0100

    Fixed Windows packaging (made it include pdfminer's cmap)

--- qt/cxfreeze_fix.py
+++ qt/cxfreeze_fix.py
@@ -1,4 +1,11 @@
 # cxfreeze has some problems detecting all dependencies.
 # This modules explicitly import those problematic modules.
 
+import os
 import markdown.etree_loader
+
+# Normally, cmap files are directly in pdfminer/cmap, but
+# with cx_freeze, pdfminer is in a zip file and it confuses
+# everything. Fortunately, we can override cmap's location
+# through environment variables, which we do here.
+os.environ['CMAP_PATH'] = 'cmap'
