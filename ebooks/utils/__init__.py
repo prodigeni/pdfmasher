e85d391205b0634f3045a378c5eea50a00c6b9d9
Virgil Dupras <hsoft@hardcoded.net>
2011-08-29 20:14:41 -0400

    Fixed a crash on HTML processing when some non-ascii entities were present.

--- ebooks/utils/__init__.py
+++ ebooks/utils/__init__.py
@@ -83,7 +83,7 @@ def entity_to_unicode(match, exceptions=[], encoding='cp1252',
         if encoding is None or num > 255:
             return check(my_unichr(num))
         try:
-            return check(chr(num).decode(encoding))
+            return check(bytes([num]).decode(encoding))
         except UnicodeDecodeError:
             return check(my_unichr(num))
     try:
