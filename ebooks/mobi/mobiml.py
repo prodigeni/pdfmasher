9d935e269a19a389bcd3245dd3d343a3a0959a63
Virgil Dupras <hsoft@hardcoded.net>
2013-03-24 10:03:06 -0400

    Changed copyright year to 2013.

--- ebooks/mobi/mobiml.py
+++ ebooks/mobi/mobiml.py
@@ -1,5 +1,5 @@
 # Copyright 2008, Marshall T. Vandegrift <llasram@gmail.cam>
-# Copyright 2011 Hardcoded Software (http://www.hardcoded.net)
+# Copyright 2013 Hardcoded Software (http://www.hardcoded.net)
 # 
 # This software is licensed under the "GPL v3" License as described in the "LICENSE" file, 
 # which should be included with this package. The terms are also available at 
